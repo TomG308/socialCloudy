30ed75b62b35c9a28775a527e9538745c0fd2615
tomg308 <tgutierrez308@gmail.com>
2019-04-17 23:04:21 -0600

    added git ignore file

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 // @route GET api/profile/test
-// @desc test post route
+// @desc test profile route
 // @access public
 
 router.get('/test', (req, res) => res.json({
