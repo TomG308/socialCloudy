a852b0dbe54d6b686946e60ed98b0bda3a33f897
tomg308 <tgutierrez308@gmail.com>
2019-04-19 01:05:05 -0600

    worked on tryig to get post requet to work

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,57 +1,59 @@
-const express = require('express')
-const router = express.Router()
-const gravatar = require('gravatar')
-const bcrypt = require('bcryptjs')
+const express = require("express");
+const router = express.Router();
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
 
 //load User Model
-const User = require('../../modles/User')
+const User = require("../../modles/User");
 
 // @route GET api/user/test
 // @desc test user route
 // @access public
 
-router.get('/test', (req, res) => res.json({
-  msg: "users works"
-}))
+router.get("/test", (req, res) =>
+  res.json({
+    msg: "users works"
+  })
+);
 
 // @route GET api/user/register
 // @descregister user
 // @access public
 
-router.post('/register', (req, res) => {
+router.post("/register", (req, res) => {
   User.findOne({
     email: req.body.email
   }).then(user => {
     if (user) {
       return res.status(400).json({
-        email: 'Email already exists'
-      })
+        email: "Email already exists"
+      });
     } else {
       const avatar = gravatar.url(req.body.email, {
-        s: '200', // size
-        r: 'pg', // rating
-        d: 'mm' // defualt
-      })
+        s: "200", // size
+        r: "pg", // rating
+        d: "mm" // defualt
+      });
 
       const newUser = new User({
         name: req.body.name,
         email: req.body.email,
         avatar: avatar,
         password: req.body.password
-      })
+      });
 
       bcrypt.genSalt(10, (err, salt) => {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err
-          newUser.password = hash
+          if (err) throw err;
+          newUser.password = hash;
           newUser
             .save()
             .then(user => res.json(user))
-            .catch(err => console.log(err))
-        })
-      })
+            .catch(err => console.log(err));
+        });
+      });
     }
-  })
-})
+  });
+});
 
 module.exports = router;
