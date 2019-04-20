2cedc7aa7214deee3f3123f5971e66421b847b9a
tomg308 <tgutierrez308@gmail.com>
2019-04-20 03:00:30 -0600

    added logic for login POST request still cant get any post requests to go through with postman

--- routes/api/user.js
+++ routes/api/user.js
@@ -56,4 +56,30 @@ router.post("/register", (req, res) => {
   });
 });
 
+// @route GET api/user/login
+// @descregister login user / returning JWT Token
+// @access public
+
+router.post("/login", (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  //find user by email
+  User.findOne({ email }).then(user => {
+    // check for user
+    if (!user) {
+      return res.status(404).json({ email: "user not found!" });
+
+      //check password
+      bcrypt.compare(password, user.password).then(isMatch => {
+        if (isMatch) {
+          res.json({ msg: "Success" });
+        } else {
+          return res.status(400).json({ password: "password incorrect" });
+        }
+      });
+    }
+  });
+});
+
 module.exports = router;
